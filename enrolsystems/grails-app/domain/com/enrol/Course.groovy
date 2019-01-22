ed45ea50f39e24cd26aef83fd1736050d3d83e93
MichalChalacinski <b7010337@my.shu.ac.uk>
2019-01-22 17:43:40 +0000

    made student controller work

---
+++ enrolsystems/grails-app/domain/com/enrol/Course.groovy
@@ -0,0 +1,28 @@
+package com.enrol
+
+class Course {
+String department
+String courseTitle
+String courseCode
+String courseLeader
+Date startDate
+Date endDate
+String description
+String studyMode
+int numberOfStudents
+double tuitionFees
+
+    static constraints = {
+courseTitle nullable: false, blank: false
+department nullable: false, blank:false
+courseLeader nullable: false, blank:false
+courseCode nullable: false, blank:false
+numberOfStudents nullable: false, blank:false, minSize: 20, maxSize: 60
+startDate nullable: false, blank:false
+endDate nullable: false, blank:false
+studyMode nullable: false, blank:false, maxSize: 20
+description nullable: false, blank:false, maxSize: 5000, widget: 'textarea'
+tuitionFees nullable: false, blank:false, scale: 2
+    }
+}
+    
